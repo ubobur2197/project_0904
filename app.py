fef36c74970ee988224af9c1040e37c02ef4fe64
Bobur <ubobur2197@gmail.com>
2026-04-09 20:44:21 +0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# 1
+roy = [1, 2, 3, 4, 5]
+print([x * 2 for x in roy])
+
+# 2
+roy = [10, 20, 30, 40]
+print([x - 5 for x in roy])
+
+# 3
+roy = ["apple", "banana", "cherry"]
+print([x.upper() for x in roy])
+
+# 4
+roy = ["salom", "dunyo", "python"]
+print([len(x) for x in roy])
+
+# 5
+roy = [3, 6, 9, 12]
+print([x ** 2 for x in roy])
